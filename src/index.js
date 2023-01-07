b59d76b63efccfe175b0c4a7f4fdd7cdb7e36465
dim419 <6696056@gmail.com>
2023-01-07 16:00:45 +0300

    feat: decision of Revers Int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    const negative = n < 0 ? n * (-1) : n
+    const incomingData = String(negative).split('');
+    const transData = incomingData.reverse();
+    const preResult = String(transData);
+    const result = Number(preResult.replace(/,/g, ''));
+    return result;
 }
